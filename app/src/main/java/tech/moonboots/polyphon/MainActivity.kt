fddda0ad881a5c389ce664da0c40f484c87fc476
Kuei <akueisara39@gmail.com>
2021-09-03 14:16:51 +0900

    Create an Android project with C++ support

---
+++ app/src/main/java/tech/moonboots/polyphon/MainActivity.kt
@@ -0,0 +1,33 @@
+package tech.moonboots.polyphon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.moonboots.polyphon.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
